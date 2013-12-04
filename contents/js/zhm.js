fcc375a4abf27f29f351616a1926992d113609cb
the3rdPoliceman <gabbymoore@gmail.com>
2013-12-04 23:42:15 +0100

    Created kontakt page, footer, and set page to open Objekte automatically

--- contents/js/zhm.js
+++ contents/js/zhm.js
@@ -5,6 +5,7 @@ function initAccordian() {
 
         var accordianHeaders = $('.accordian-header');
         accordianHeaders.click(function() {
+            $('.accordian-header').removeClass('accordian-header-selected');
             var accordianContent = $(this).next().next();
 
             if (accordianContent.is(":visible")) {
@@ -12,8 +13,45 @@ function initAccordian() {
             } else {
                 allPanels.slideUp();
                 accordianContent.slideDown();
+                $(this).addClass('accordian-header-selected');
             }
+
+            unsqueezeObjektePanel();
+            triggerResizeMap();
             return false;
         });
+
+        openObjekte();
+
+        $('.more-button-panel').click(function() {
+            unsqueezeObjektePanel();
+        });
+
+    });
+}
+
+function triggerResizeMap() {
+    google.maps.event.trigger(map, "resize");
+}
+
+function openObjekte() {
+    $('#objekte-header').addClass('accordian-header-selected').next().next().slideDown();
+}
+
+function unsqueezeObjektePanel() {
+    $('#objekte-content').removeClass('objekte-squeezed');
+    $('.more-button-panel').remove();
+}
+
+function updateColors() {
+    var allPanels = $('.accordian-header');
+
+    allPanels.each(function(index) {
+        var accordianContent = $(this).next().next();
+
+        var visible = accordianContent.is(':visible');
+        $(this).toggleClass('accordian-header-selected', visible);
+
+        alert($(this).html() + visible);
     });
 }
